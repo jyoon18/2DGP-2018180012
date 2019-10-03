c9a69d5f528290a4e05c8ab6b429f6435c1bb994
Kimjeongyoon <mmh2018@naver.com>
2019-10-03 23:56:48 +0900

    개발과제 2018180012 김정윤
    맵, 점프구현, 플레이어블 캐릭터 추가 및 추가 캐릭터 좌표 구현

--- Develop/character_runs.py
+++ Develop/character_runs.py
@@ -1,38 +1,36 @@
 from pico2d import *
 open_canvas(1280,600)
-grass = load_image('map04_1.png')
+grass = load_image('map03_3.png')
 character = load_image('running1.png')
-
-#MAP_WIDTH, MAP_HEIGHT = 800, 315
 
 frame=0
 x=800
+toggle = True
 
-#x, y = MAP_WIDTH, MAP_HEIGHT
+while (x>-800):
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            jump = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_f:
+                toggle = False
 
-while(x<1600):
     clear_canvas()
     grass.draw(x,300)
-    character.clip_draw(frame*160,0,160,160,90,90)
+    if toggle == False:
+        y=250
+        character.clip_draw(frame * 160, 0, 160, 160, 90, y)
+        frame = (frame + 1) % 4
+        toggle=True
+    elif (toggle == True):
+        character.clip_draw(frame * 160, 0, 160, 160, 90, 90)
+        frame = (frame + 1) % 4
     update_canvas()
-    frame = (frame + 1) % 4
     x-=2
-    delay(0.08)
     get_events()
+    delay(0.1)
 
-while(x>0):
-    clear_canvas()
-    grass.draw(x, 300)
-    character.clip_draw(frame*160,0,160,160,90,90)
-    update_canvas()
-    frame = (frame + 1) % 4
-    x += 2
-    delay(0.08)
-    get_events()
-
-#while (xlen < 1600):
- #   clear_canvas()
 
 
 close_canvas()
-
