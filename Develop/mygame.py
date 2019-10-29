b3a349738af0aefcee18b5414182b46c91b65de8
Kimjeongyoon <mmh2018@naver.com>
2019-10-29 20:53:51 +0900

    ㅁㅇ
    ㄹㅁㅇ

--- Develop/mygame.py
+++ Develop/mygame.py
@@ -6,5 +6,5 @@ import start_state
 
 pico2d.open_canvas(1280, 600)        # 피코투디로 캔버스를 열어준다
 game_framework.run(start_state)     # start_state 상태로 game_framework가 돌아가게 해준다
-                                    # 그럼 start_state -> title_state-> main_state상태로 접어들고 main_state에서 pause이벤트가 일어나게 해줌
+
 pico2d.close_canvas()
