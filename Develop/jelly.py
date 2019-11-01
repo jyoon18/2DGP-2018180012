8606b88975a034ff3e5f008cec372d1e62b1e646
Kimjeongyoon <mmh2018@naver.com>
2019-11-02 00:04:18 +0900

    젤리수정
    젤리 수정

--- Develop/jelly.py
+++ Develop/jelly.py
@@ -1,7 +1,14 @@
 from pico2d import *
 
-class Jelly:
+class Small_Jelly_lv1:
     def __init__(self):
-        self.x, self.y = 0
+        self.x, self.y = 0, 0
         self.speed = 0
-        self.image = load_image()
+        self.image = load_image('Level1_jelly.png')
+        self.frame = 0
+
+    def update(self):
+        self.frame = (self.frame + 1) % 3
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 80, 0, 80, 80, self.x, self.y)
