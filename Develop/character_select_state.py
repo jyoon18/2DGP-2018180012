8e312ea02b419dbf934ca6c092e45a67507dc726
Kimjeongyoon <mmh2018@naver.com>
2019-11-01 23:54:55 +0900

    젤리 클래스 파일 추가
    할일 추가 & 캐릭터 선택창추가 & 선택창에서 캐릭터가 움직이게 수정

--- Develop/character_select_state.py
+++ Develop/character_select_state.py
@@ -5,15 +5,46 @@ import main_state
 
 
 image = None
+character1 = None
+character2 = None
+
+class Select1:
+    def __init__(self):
+        self.x, self.y = 400, 200
+        self.frame = 0
+        self.image = load_image('character1_select_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 168, 0 , 160, 160, self.x, self.y)
+
+class Select2:
+    def __init__(self):
+        self.x, self.y = 800, 300
+        self.frame = 0
+        self.image = load_image('character2_select_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 363, 0, 363, 363, self.x, self.y)
+
 
 def enter():
-    global image
+    global image, character1, character2
     image = load_image('character_select_window.png')
-
+    character1 = Select1()
+    character2 = Select2()
 
 def exit():
-    global image
+    global image, character1, character2
     del image
+    del character1
+    del character2
+
 
 def handle_events():
     events = get_events()
@@ -28,15 +59,26 @@ def handle_events():
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_2):
                 game_framework.change_state(main_state)
 
+def update():
+    global character1, character2
+    character1.update()
+    character2.update()
+    pass
+
 def draw():
     clear_canvas()
     image.draw(640,300)
+
+    character1.draw()
+    character2.draw()
+
+
     update_canvas()
+    delay(0.1)
     pass
 
 
-def update():
-    pass
+
 
 
 def pause():
---
+++ Develop/jelly.py
@@ -0,0 +1,7 @@
+from pico2d import *
+
+class Jelly:
+    def __init__(self):
+        self.x, self.y = 0
+        self.speed = 0
+        self.image = load_image()
