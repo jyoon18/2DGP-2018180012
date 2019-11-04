3b529d89cb5ea32d5cd78c576034dd474503972c
Kimjeongyoon <mmh2018@naver.com>
2019-11-05 00:02:54 +0900

    게임 월드 작성 & 할거 추가
    게임월드 작성 & 할거 추가

--- Develop/game_world.py
+++ Develop/game_world.py
@@ -3,3 +3,9 @@
 
 objects = [[], []]          # 게임 월드에 담겨있는 모든 객체들을 담고있는 리스트,
                             # Drawing Layer에 따라서 분류
+
+def add_object(o, layer):
+    objects[layer].append(o)        # 게임월드에 객체를 추가
+
+def add_objects(l, layer):
+    objects[layer] += 1             # 게임월드에 객체들을 추가
