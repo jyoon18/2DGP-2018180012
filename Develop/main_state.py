eb30fc34ffb661fbd552551e33cb75e175b51dc6
Kimjeongyoon <mmh2018@naver.com>
2019-11-07 02:23:16 +0900

    히트박스 추가
    추가

--- Develop/main_state.py
+++ Develop/main_state.py
@@ -73,6 +73,7 @@ def update():
     for game_object in game_world.all_objects():
         game_object.update()
 
+
     pass
 
 def draw():
@@ -87,7 +88,16 @@ def draw():
 
 def collide(a, b):
     left_a, bottom_a, right_a, top_a = a.get_bb()
-    
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
 
 
 
