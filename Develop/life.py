1cde2d2f26fe7a19cb33d9dd68e7f6a6cafc20d5
Kimjeongyoon <mmh2018@naver.com>
2019-11-08 22:41:13 +0900

    하트 추가 완료
    하지만 수정되어야 할 필요가 있음

--- Develop/life.py
+++ Develop/life.py
@@ -2,12 +2,16 @@ from pico2d import *
 
 class Life:
     def __init__(self):
-        self.x, self.y = 840, 500
+        self.x, self.y = 950, 540
         self.image = load_image('used_image/life.png')
-        self.interval_distance = 60
-        
+        self.interval_distance = 65
+
     def update(self):
-        self.x += self.interval_distance
+        pass
 
     def draw(self):
         self.image.draw(self.x, self.y)
+        self.image.draw(self.x + self.interval_distance, self.y)
+        self.image.draw(self.x + self.interval_distance * 2, self.y)
+        self.image.draw(self.x + self.interval_distance * 3, self.y)
+        self.image.draw(self.x + self.interval_distance * 4, self.y)
