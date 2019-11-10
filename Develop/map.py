8e42ab40ce96e1e2947f99fc7013a304f33fe575
Kimjeongyoon <mmh2018@naver.com>
2019-11-10 21:40:24 +0900

    맵 스크롤링 도전중
    맵 스크롤링 도전중

--- Develop/map.py
+++ Develop/map.py
@@ -2,11 +2,18 @@ from pico2d import *
 
 class Maps:
     def __init__(self):
-        self.image = load_image('used_image/map02_3.png')
+        self.image_first = load_image('used_image/map02_3.png')
+        self.image_second = load_image('used_image/map02_4.png')
         self.x, self.y = 640,300
         self.frame = 0
-    def draw(self):
-        self.image.draw(self.x, self.y)
 
     def update(self):
         self.x -= 1
+        if self.x == 0:
+            self.x = 640
+
+
+    def draw(self):
+        self.image_first.draw(self.x, self.y)
+        if self.x == 0:
+            self.image_second(self.x, self.y)
