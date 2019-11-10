8e409d46aab3f63d3ed1440f347ca4ffc7b6db5a
Kimjeongyoon <mmh2018@naver.com>
2019-11-10 21:46:43 +0900

    할거 추가
    할 거 추가

--- Develop/map.py
+++ Develop/map.py
@@ -4,7 +4,7 @@ class Maps:
     def __init__(self):
         self.image_first = load_image('used_image/map02_3.png')
         self.image_second = load_image('used_image/map02_4.png')
-        self.x, self.y = 640,300
+        self.x, self.y = 640, 300
         self.frame = 0
 
     def update(self):
